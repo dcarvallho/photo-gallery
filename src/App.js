134e54fe69cb633d8c05716597ed407cfaf99f96
Davi Augusto <davi_augusto11@hotmail.com>
2023-02-24 14:31:19 -0300

    Testando aplicação

--- src/App.js
+++ src/App.js
@@ -2,22 +2,7 @@ import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Testando APP</h1>
   );
 }
 
